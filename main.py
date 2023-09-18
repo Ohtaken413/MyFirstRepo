1b047541e734d98325b64b4cee8699bfdab03c26
Ohtaken413 <Noboru.Ohtake@gmail.com>
2023-09-18 23:02:20 +0900

    足し算、引き算、掛け算をするプログラム

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 def add(a, b):
-    return a+ b
+    return a + b
 
 def sub(a, b):
     return a - b
 
-def main():
-    print(add(1, 2))
+def mul(a, b):
+    return a * b
 
+def main():
+    print("{} + {}",format(1, 2, add(1, 2)))
+    print("{} + {}",format(2, 1, sub(2, 1)))
+    print("{} + {}",format(2, 2, mul(2, 2)))
+    
 if __name__ == '__main__':
     main()
